1339d9ce57c02cfe33f3b632ddd2236d1edc0ce3
Grant Hutchins <nertzy@gmail.com>
2011-05-01 20:06:48 -0400

    More of chapter 4 Sinatra project.

--- projects/webapp.rb
+++ projects/webapp.rb
@@ -4,7 +4,43 @@ get "/" do
   "Hello, world!"
 end
 
-# 
+get "/how-are-you" do
+  "I'm fine, thanks for asking."
+end
+
+get "/hello/:name" do
+  "Hello, " + params[:name] + "!"
+end
+
+get "/:x/followed-by/:y" do
+  params[:x] + " " + params[:y]
+end
+
+get "/:x/plus/:y" do
+  Integer(params[:x]) + Integer(params[:y])
+end
+
+def hi(name)
+  "Hi, " + name + "!"
+end
+
+get "/hi/:name" do
+  name = params[:name]
+  hello(name)
+end
+
+get "/:word/twice" do
+  params[:word] + "<br>" + params[:word]
+end
+
+get "/:word/:number/times" do
+  (params[:word] + " ") * Integer(params[:number])
+end
+
+get "/hi/:name/:number/times" do
+  (hi(params[:name]) + " ") * Integer(params[:number])
+end
+
 # get '/' do
 #   "Here's Google! " + link_to("google", "http://www.google.com")
 # end
